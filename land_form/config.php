b0fe858f62f57ae727366a22575f82c44e67939e
Negite <47744442+Negite@users.noreply.github.com>
2019-04-16 12:53:32 +0300

    Delete config.php

--- land_form/config.php
+++
@@ -1,9 +0,0 @@
-﻿<?
-// Блок 1: Параметры для подключения
-$db_host = 'localhost'; 
-$db_user = 'root'; // Логин БД
-$db_password = ''; // Пароль БД
-$db_base = 'land_form'; // Имя БД
-
-mail("negite@mail.ru", "$name", "$message", "From: negite@yandex.ru");
-?>
